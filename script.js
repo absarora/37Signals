c51e1bf81a7b4dab498336633d5d009d621679ef
absingharora <absingharora@gmail.com>
2013-10-07 22:12:39 -0700

    37SignalsClone

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$("#box1").mouseenter(function() {
+	$("#arrow1").show(10);
+	});
+	$(".box_container").mouseleave(function() {
+	$("#arrow1").hide(10);
+	});
+});
+$(document).ready(function(){
+	$("#box2").mouseenter(function() {
+	$("#arrow2").show(10);
+	});
+	$(".box_container").mouseleave(function() {
+	$("#arrow2").hide(10);
+	});
+});
+$(document).ready(function(){
+	$("#box3").mouseenter(function() {
+	$("#arrow3").show(10);
+	});
+	$(".box_container").mouseleave(function() {
+	$("#arrow3").hide(10);
+	});
+});
