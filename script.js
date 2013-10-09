b687af34370839926330e0732a2acfe633f90dea
absingharora <absingharora@gmail.com>
2013-10-08 22:35:23 -0700

    changes made to jQuery

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 $(document).ready(function(){
+/*-------------BOX 1--------------------*/
 	$("#box1").mouseenter(function() {
 	$("#arrow1").show(10);
+	
+	$("#basecamp").show(10);
+	$("#head").hide(10);
+	$("#p2").show(10);
+	$("#p1").hide(10);
 	});
 	$(".box_container").mouseleave(function() {
 	$("#arrow1").hide(10);
+	
+	$("#basecamp").hide(10);
+	$("#head").show(10);
+	$("#p1").show(10);
+	$("#p2").hide(10);
 	});
-});
-$(document).ready(function(){
+/*-------------BOX 2---------------------*/
 	$("#box2").mouseenter(function() {
 	$("#arrow2").show(10);
+	
+	$("#highrise").show(10);
+	$("#head").hide(10);
+	$("#p3").show(10);
+	$("#p1").hide(10);
 	});
 	$(".box_container").mouseleave(function() {
 	$("#arrow2").hide(10);
+	
+	$("#highrise").hide(10);
+	$("#head").show(10);
+	$("#p1").show(10);
+	$("#p3").hide(10);
 	});
-});
-$(document).ready(function(){
+/*-------------BOX 3---------------------*/
 	$("#box3").mouseenter(function() {
 	$("#arrow3").show(10);
+	
+	$("#campfire").show(10);
+	$("#head").hide(10);
+	$("#p4").show(10);
+	$("#p1").hide(10);
 	});
 	$(".box_container").mouseleave(function() {
 	$("#arrow3").hide(10);
+	
+	$("#campfire").hide(10);
+	$("#head").show(10);
+	$("#p1").show(10);
+	$("#p4").hide(10);
 	});
 });
